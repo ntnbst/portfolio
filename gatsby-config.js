00df27227f785478688b279a75e1fb3e5b55ff75
Nitin Bisht <nitin.bisht47@gmail.com>
2018-03-14 14:01:37 +0530

    Added Favicon, Edited README

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,25 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-};
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+  ],
+}

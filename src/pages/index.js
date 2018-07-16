e261faea5157c1e93c69ac44b89c94a6260cd3f2
Nitin Bisht <nitin.bisht@sqrrl.in>
2018-07-16 21:31:50 +0530

    updated background color && particles data

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import Footer from '../components/Footer/footer'
 import '../styles/styles.css'
 import Particles from 'react-particles-js'
-import Typed from 'react-typed';
+import Typed from 'react-typed'
 
 const videoStyle = {
   // position: 'fixed',
@@ -28,126 +28,104 @@ const videoStyle = {
 const IndexPage = () => (
   <div>
     <main id="main-desc">
-      <h1 id="introStyle">
-        <Typed strings={['I am Nitin Bisht']} typeSpeed={40} />
-      </h1>
-      <h3 id="small-heading"><Typed strings={['I make excellent, modern websites.', 'I make excellent, modern web applications.']} backspeed={100} typeSpeed={30} startDelay={1500}/></h3>
+      <h1 id="introStyle">I am Nitin Bisht</h1>
+      <h3 id="small-heading">
+        <Typed
+          strings={[
+            'I make excellent, modern websites.',
+            'I make excellent, modern web applications',
+          ]}
+          backspeed={100}
+          typeSpeed={30}
+          startDelay={100}
+        />
+      </h3>
     </main>
 
     <Particles
-        params={{
-          "particles": {
-            "number": {
-              "value": 120,
-              "density": {
-                "enable": true,
-                "value_area": 868.0624057955
-              }
-            },
-            "color": {
-              "value": "#fff"
-            },
-            "shape": {
-              "type": "circle",
-              "stroke": {
-                "width": 0,
-                "color": "#000000"
-              },
-              "polygon": {
-                "nb_sides": 5
-              }
-            },
-            "opacity": {
-              "value": 0.5,
-              "random": true,
-              "anim": {
-                "enable": false,
-                "speed": 1,
-                "opacity_min": 0.1,
-                "sync": false
-              }
-            },
-            "size": {
-              "value": 3,
-              "random": true,
-              "anim": {
-                "enable": false,
-                "speed": 40,
-                "size_min": 0.1,
-                "sync": false
-              }
-            },
-            "line_linked": {
-              "enable": true,
-              "distance": 150,
-              "color": "#fff",
-              "opacity": 0.4,
-              "width": 1
-            },
-            "move": {
-              "enable": true,
-              "speed": 5,
-              "direction": "none",
-              "random": false,
-              "straight": false,
-              "out_mode": "out",
-              "bounce": false,
-              "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
-              }
-            }
+      params={{
+        particles: {
+          number: {
+            value: 100,
+            density: { enable: true, value_area: 868.0624057955 },
           },
-          "interactivity": {
-            "detect_on": "canvas",
-            "events": {
-              "onhover": {
-                "enable": false,
-                "mode": "grab"
-              },
-              "onclick": {
-                "enable": true,
-                "mode": "push"
-              },
-              "resize": false
-            },
-            "modes": {
-              "grab": {
-                "distance": 400,
-                "line_linked": {
-                  "opacity": 1
-                }
-              },
-              "bubble": {
-                "distance": 400,
-                "size": 40,
-                "duration": 2,
-                "opacity": 8,
-                "speed": 3
-              },
-              "repulse": {
-                "distance": 200,
-                "duration": 0.4
-              },
-              "push": {
-                "particles_nb": 4
-              },
-              "remove": {
-                "particles_nb": 2
-              }
-            }
+          color: { value: '#fff' },
+          shape: {
+            type: 'circle',
+            stroke: { width: 0, color: '#000000' },
+            polygon: { nb_sides: 5 },
           },
-          "retina_detect": true
-        }}
-        style={{
-        "position": "absolute",
-        "top": 0,
-        "width": "100%",
-        "height": "100%",
-        "zIndex": "-1"
+          opacity: {
+            value: 0.5,
+            random: true,
+            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+          },
+          line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#fff',
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            enable: true,
+            speed: 5,
+            direction: 'none',
+            random: false,
+            straight: false,
+            out_mode: 'out',
+            bounce: false,
+            attract: { enable: false, rotateX: 600, rotateY: 1200 },
+          },
+        },
+        interactivity: {
+          detect_on: 'canvas',
+          events: {
+            onhover: { enable: false, mode: 'grab' },
+            onclick: { enable: true, mode: 'push' },
+            resize: true,
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1,
+              },
+            },
+            bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+            push: {
+              particles_nb: 4,
+            },
+            remove: {
+              particles_nb: 2,
+            },
+          },
+        },
+        retina_detect: true,
       }}
-        />
+      style={{
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        height: '100%',
+        zIndex: '-1',
+      }}
+    />
     <Footer />
   </div>
 )
